043d4200e1dcc3724253256f954f7d26b142efe6
Priyankar01 <priyankardutta001@gmail.com>
2025-03-31 20:34:49 +0530

    test backend

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -24,6 +24,7 @@ export async function POST(req: Request) {
 
 		return NextResponse.json(uploadResult);
 	} catch (error) {
+		console.log(error);
 		return NextResponse.json({ error: 'Upload failed' }, { status: 500 });
 	}
 }
